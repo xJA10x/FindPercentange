999e9f0f7dcbac52dec3b766926795905dcfca29
xJA10x <>
2019-09-17 16:49:05 -0400

    Working files

---
+++ Percentage.java
@@ -0,0 +1,15 @@
+/***************************************
+
+Percentage class
+
+****************************************/
+
+abstract class Percentage {
+
+  // Builds abstract method.
+  // Takes two parameters,
+  // local varaibles in the main class
+  // price and discount.
+  public abstract double findPercentage(double price, double discount);
+
+}
